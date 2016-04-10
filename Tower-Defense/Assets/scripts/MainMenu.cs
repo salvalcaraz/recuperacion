72db88192c25b214e09a7e3fc7e3d9af2f6d8f25
salvalcaraz <s.alcarazg@gmail.com>
2016-04-10 18:56:37 +0200

    Menu principal
    Creacion de nueva escena con menu.

---
+++ Tower-Defense/Assets/scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
