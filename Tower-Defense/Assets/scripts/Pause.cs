011ede9520f0f62aaef9580387665a91b496630a
salvalcaraz <s.alcarazg@gmail.com>
2016-04-10 20:19:29 +0200

    Menu de pausa
    Menu de pausa con opciones:
    -resume
    -quit
    -main menu
    -mute

---
+++ Tower-Defense/Assets/scripts/Pause.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+    GameObject PauseMenu;
+    bool paused;
+    bool muted;
+    [SerializeField]
+
+
+
+
+	void Start () {
+        paused = false;
+        PauseMenu = GameObject.Find("PauseMenu");
+	
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+        }
+        if (paused)
+        {
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else if (!paused)
+        {
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+        if (muted)
+        {
+            AudioListener.volume = 0;
+        }
+        else if (!muted)
+        {
+            AudioListener.volume = 1;
+        }
+	}
+
+    public void Resume()
+    {
+        paused = false;
+    }
+
+    public void MainMenu()
+    {
+        Application.LoadLevel(0);
+    }
+
+    public void Mute()
+    {
+        muted = !muted;
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+}
