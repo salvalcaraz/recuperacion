f43d30e36cf9c40679771394cc073d2707a9e68a
salvalcaraz <s.alcarazg@gmail.com>
2016-04-10 17:30:12 +0200

    Spawn de monstruos
    dos tipos de spawn:
    -pelotas (1 vida-5sec)
    -Cruces (3vidas-8sec)

---
+++ Tower-Defense/Assets/scripts/Spawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject monsterPrefab;
+    public float interval = 3;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnNext", interval, interval);
+    }
+
+    void SpawnNext()
+    {
+        Instantiate(monsterPrefab, transform.position, Quaternion.identity);
+    }
+}
