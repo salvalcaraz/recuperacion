c94a01077fcebe78a679c3adb427082c8264f70f
salvalcaraz <s.alcarazg@gmail.com>
2016-04-10 17:15:56 +0200

    enemigos
    enemigos

---
+++ Tower-Defense/Assets/scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+    TextMesh tm;
+
+    void Start()
+    {
+        tm = GetComponent<TextMesh>();
+    }
+
+    void Update()
+    {
+        transform.forward = Camera.main.transform.forward;
+    }
+
+    public int current()
+    {
+        return tm.text.Length;
+    }
+
+    public void decrease()
+    {
+        if (current() > 1)
+            tm.text = tm.text.Remove(tm.text.Length - 1);
+        else
+            Destroy(transform.parent.gameObject);
+    }
+}
---
+++ Tower-Defense/Assets/scripts/Monster.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour
+{
+
+    void Start()
+    {
+
+        GameObject castle = GameObject.Find("Castle");
+        if (castle)
+            GetComponent<NavMeshAgent>().destination = castle.transform.position;
+    }
+
+    void OnTriggerEnter(Collider co)
+    {
+        if (co.name == "Castle")
+        {
+            co.GetComponentInChildren<Health>().decrease();
+            Destroy(gameObject);
+        }
+
+
+    }
+}
